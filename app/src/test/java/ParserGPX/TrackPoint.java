3fcff735530aafc24f42216788a630ae00298f53
Guilhem <45591444+Driftix@users.noreply.github.com>
2022-03-14 11:18:53 +0100

    Added parser

---
+++ app/src/test/java/ParserGPX/TrackPoint.java
@@ -0,0 +1,23 @@
+package ParserGPX;
+/**
+ * Created by artz on 24/12/14.
+ */
+public class TrackPoint {
+
+    private double latitude;
+    private double longitude;
+
+    public TrackPoint(double latitude, double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+}
